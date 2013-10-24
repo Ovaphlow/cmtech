fc3575516093f21f05b38d74b7f1e50e9a2ac7d2
Ovaphlow <jsfreecss@163.com>
2013-10-24 17:46:43 +0800

    多文件选择上传
    Signed-off-by: Ovaphlow <jsfreecss@163.com>

--- archieve/shangchuan.py
+++ archieve/shangchuan.py
@@ -5,14 +5,14 @@ import mysql.connector
 from flask import request, render_template, redirect
 from werkzeug import secure_filename
 
+
 def get(id):
-    sql = (
-        'SELECT * FROM dangan '
-        'WHERE id=%s'
-    )
-    param = (
-        id,
-    )
+    cat = request.args.get('cat', '1')
+    sql = '''
+        SELECT * FROM dangan
+        WHERE id=%s
+    '''
+    param = (id,)
     cnx = mysql.connector.Connect(**globalvars.cnx_cfg)
     cursor = cnx.cursor()
     cursor.execute(sql, param)
@@ -26,114 +26,36 @@ def get(id):
     fp = globalvars.get_file_path(id)
     lp = '/saomiao/%s' % (id,)
     return render_template('shangchuan.boot.html',
-        row=row, 
-        id=id, 
+        row=row,
+        id=id,
         filepath=fp,
-        link = lp
+        link = lp,
+        cat = cat
     )
 
-def post(id):
-    if request.method == 'POST':
-        print 'request.files', request.files
 
-        fp = globalvars.get_file_path1(id)
-        file_list = []
-        files_list = request.files.getlist('file1')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl1 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file2')
-        print files_list[0]
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl2 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file3')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl3 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file4')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl4 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file5')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl5 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file6')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl6 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file7')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl7 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file8')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl8 = globalvars.join_file_name(';', file_list)
-        file_list = []
-        files_list = request.files.getlist('file9')
-        for f in files_list:
-            if f and globalvars.check_ext(f.filename):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(fp, filename))
-                file_list.append(filename)
-        fl9 = globalvars.join_file_name(';', file_list)
-        sql = 'SELECT COUNT(*) FROM wenjian WHERE id=%s'
-        param = (id, )
-        cnx = mysql.connector.connect(**globalvars.cnx_cfg)
-        cursor = cnx.cursor()
-        cursor.execute(sql, param)
-        data = cursor.fetchall()
-        if data[0][0] == 0:
-            sql = (
-                'INSERT INTO wenjian '
-                'VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
-            )
-            param = (id, fl1, fl2, fl3, fl4, fl5, fl6, fl7, fl8, fl9)
-            #cursor.execute(sql, param)
-        else:
-            sql = (
-                'UPDATE wenjian '
-                'SET '
-                'cat_1=%s,cat_2=%s,cat_3=%s,cat_4=%s,cat_5=%s,cat_6=%s,'
-                'cat_7=%s,cat_8=%s,cat_9=%s '
-                'WHERE id=%s'
-            )
-            param = (fl1, fl2, fl3, fl4, fl5, fl6, fl7, fl8, fl9, id)
-            #cursor.execute(sql, param)
-        #cnx.commit()
-        cursor.close()
-        cnx.close()
-        return redirect('/shangchuan/%s' % (id,))
+def post(id):
+    #print 'headers', request.headers
+    cat = request.args.get('cat', '1')
+    #print 'id', id
+    print id, cat
+    #print 'request.value', request.values
+    aid = globalvars.get_aid(id)
+    fp = '%s\\%s' % (globalvars.G_UPLOAD_PATH, aid)
+    globalvars.check_path(fp)
+    cnx = mysql.connector.Connect(**globalvars.cnx_cfg)
+    cursor = cnx.cursor()
+    for f in request.files.getlist('Filedata'):
+        if f and globalvars.check_ext(f.filename):
+            fn = secure_filename(f.filename)
+            fp = os.path.join(fp, fn)
+            f.save(fp)
+            sql = '''
+                INSERT INTO wenjian (id,aid,LeiBie,WenJianMing)
+                VALUES(0,%s,%s,%s)
+            '''
+            param = (id, cat, fn)
+            print id, cat, fn
+            cursor.execute(sql, param)
+    cnx.commit()
+    return '[%s]上传完成'  % (fn)
