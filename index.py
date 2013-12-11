431a434fb2ae5ac25480ad1a7fcd107125f43af3
Ovaphlow <jsfreecss@163.com>
2013-12-11 14:49:36 +0800

    修改首页显示内容

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 # -*- coding=UTF-8 -*-
 from flask.views import MethodView
 
-import globalvars
-
 
 class Index(MethodView):
     def get(self):
         import mysql.connector
         from flask import redirect, render_template, session
+        import globalvars
 
         if not 'id' in session:
             return redirect('/login')
@@ -19,13 +18,33 @@ class Index(MethodView):
         cursor = cnx.cursor()
         cursor.execute(sql)
         data = cursor.fetchall()
+        sql = '''
+            SELECT COUNT(*) FROM dangan
+            UNION
+            SELECT COUNT(*) FROM (
+                SELECT d.id
+                FROM dangan d
+                INNER JOIN wenjian w
+                ON d.id=w.aid
+                GROUP BY d.id
+            ) AS a
+            UNION
+            SELECT COUNT(*) FROM wenjian
+        '''
+        cursor.execute(sql)
+        data_count = cursor.fetchall()
         cursor.close()
         cnx.close()
-        return render_template('index.html', data=data, User=session['user'])
+        return render_template('index.html',
+                               data=data,
+                               User=session['user'],
+                               data_count = data_count
+        )
 
     def post(self):
         import mysql.connector
         from flask import request, redirect
+        import globalvars
 
         sql = 'SELECT id FROM dangan WHERE DangAnHao=%s OR ShenFenZheng=%s'
         param = (request.form['id'], request.form['id'])
