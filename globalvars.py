27a75ddac87743dea62eeef38881a0842dd86fd1
Ovaphlow <jsfreecss@163.com>
2014-01-13 14:24:33 +0800

    修改身份证计算函数变量名

--- globalvars.py
+++ globalvars.py
@@ -135,10 +135,10 @@ def idcard_convert(idcard_15):
     ai = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2']
     idcard_18 = '%s19%s' % (idcard_15[0:6], idcard_15[6:15])
     i = 0
-    sum = 0
+    sum_a = 0
     for c in idcard_18:
         sum_t = int(c) * int(wi[i])
-        sum += sum_t
+        sum_a += sum_t
         i += 1
-    idcard_18 = '%s%s' % (idcard_18, ai[sum % 11])
+    idcard_18 = '%s%s' % (idcard_18, ai[sum_a % 11])
     return idcard_18
