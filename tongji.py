2e99ae1375a5b5394b227ba36b8a3c4ce11b97d8
Ovaphlow <jsfreecss@163.com>
2014-01-22 19:52:45 +0800

    mf

--- tongji.py
+++ tongji.py
@@ -1,13 +1,38 @@
 # -*- coding=UTF-8 -*-
 from flask.views import MethodView
 
+"""
+统计
+：各用户操作总数对比
+：按月份各用户操作总数曲线
+：按月份各用户扫描总数曲线
+：按月份各用户添加档案总数曲线
+"""
 
 class TongJi(MethodView):
     def get(self):
-        from flask import render_template, session, redirect
+        from flask import render_template, session
+        from globalvars import connect_db, close_db
 
         if not 'user_id' in session:
             return redirect('/login')
-
-        return render_template('tongji.html', User = session['user_name'])
-
+        sql = '''
+            select
+                u.id,u.mingcheng,count(c.id)
+            from
+                `cm_archieve`.user u
+                left join
+                `cm_archieve`.caozuo_jilu c
+                on
+                u.id=c.yh_id
+            group by
+                u.id
+        '''
+        cnx = connect_db()
+        cursor = cnx.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return render_template('tongji.html',
+            User = session['user_name'],
+            counter_1 = result,
+        )
