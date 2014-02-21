16e06b05bb928b29cf6df33d7f458c15d5a6292b
Ovaphlow <jsfreecss@163.com>
2014-02-21 12:23:56 +0800

    调整结构

--- archieve/static/swf/saveImage.aspx
+++
@@ -1,1 +0,0 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="saveImage.aspx.cs" Inherits="saveImage" %>
--- eiac-hpdw/static/js/funx.js
+++
@@ -1,9 +0,0 @@
-function AddElement(id, name){
-  var mytype, TemO = document.getElementById(id);
-  var newInput = document.createElement("input");
-  newInput.type="file";
-  newInput.name=name;
-  TemO.appendChild(newInput);
-  var newline = document.createElement("br");
-  TemO.appendChild(newline);
-}
