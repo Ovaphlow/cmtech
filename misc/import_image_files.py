8bb895861feb48f99775707eb32e17a5f00084d6
Ovaphlow <jsfreecss@163.com>
2014-02-25 15:50:49 +0800

    导入文件

--- misc/import_image_files.py
+++ misc/import_image_files.py
@@ -1,7 +1,9 @@
+#! /usr/bin/env python
 # -*- coding=UTF-8 -*-
 
-source_path = 'd:\\test1'
-target_path = 'd:\\test2'
+source_path = '/home/ovaphlow/srcode/test1'
+target_path = '/home/ovaphlow/srcode/test2'
+path_divider = '/'
 
 db_param = {
     'user': 'cmtech',
@@ -10,7 +12,7 @@ db_param = {
     'database': 'cm_archieve',
 }
 
-def connect_db():
+def open_db():
     import mysql.connector
 
     return mysql.connector.Connect(**db_param)
@@ -40,22 +42,56 @@ def rename_dir():
         p = os.path.join(source_path, item)
         if os.path.isdir(p):
             name = item.split('-')[len(item.split('-')) - 1]
-            print u'重命名:', p, '->', os.path.join(source_path, name)
             os.rename(p, os.path.join(source_path, name))
 
 
-def import_files(path):
+def copy_files(path):
     import os
+    import shutil
 
     for item in os.listdir(path):
         p = os.path.join(path, item)
         if os.path.isdir(p):
-            print u'目录:', p
-            import_files(p)
+            t = os.path.join(target_path, item)
+            if os.path.exists(t):
+                print 'Exists directory:', t
+                continue
+            os.mkdir (t)
+            copy_files(p)
         else:
-            print u'文件:', p
-
+            dir_name = os.path.split(p)[0].split(path_divider)
+            dir_name = dir_name[len(dir_name) - 1]
+            t = os.path.join(target_path, dir_name, item)
+            if os.path.isfile(t):
+                print 'Exists file:', t
+                continue
+            shutil.copyfile(p, t)
+            sql = ('select id from dangan where danganhao=%(archieve_id)s')
+            param = {'archieve_id': dir_name}
+            cnx = open_db()
+            cursor = cnx.cursor()
+            cursor.execute(sql, param)
+            result = cursor.fetchall()
+            if result == None:
+                print 'No archieve #', dir_name, 'exists'
+                continue
+            sql = ('insert into wenjian '
+                '(aid, Leibie, WenJianMing, client_access) '
+                'values '
+                '(%(archieve_id)s,'
+                '%(cat)s,'
+                '%(file_name)s,'
+                '%(client_access)s)')
+            param = {
+                'archieve_id': result[0][0],
+                'cat': 9,
+                'file_name': item,
+                'client_access': 0
+            }
+            cursor.execute(sql, param)
+            cnx.commit()
+            close_db(cursor, cnx)
 
 if __name__ == '__main__':
     rename_dir()
-    import_files(path=source_path)
+    copy_files(source_path)
