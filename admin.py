3a4a83f787ebfacb82fdefaa95ba5215ea04fc4a
ovaphlow <jsfreecss@163.com>
2014-05-11 22:26:32 +0800

    添加修改用户信息功能

--- admin.py
+++ admin.py
@@ -12,3 +12,65 @@ class Home(MethodView):
             return redirect('/logout')
         return render_template('admin_home.html',
             user_name=session['user_name'])
+
+
+class UserList(MethodView):
+    def get(self):
+        if not session['user_account'] in G_ADMIN_USER:
+            return redirect('/logout')
+        sql = '''
+            select *
+            from user
+        '''
+        cnx = connect_db()
+        cursor = cnx.cursor()
+        cursor.execute(' '.join(sql.split()))
+        res = cursor.fetchall()
+        return render_template('admin_user_list.html',
+            user_name=session['user_name'],
+            data=res)
+
+
+class User(MethodView):
+    def get(self):
+        if not session['user_account'] in G_ADMIN_USER:
+            return redirect('/logout')
+        user_id = request.args.get('user_id')
+        sql = '''
+            select *
+            from user
+            where id=%(user_id)s
+        '''
+        param = {'user_id': user_id}
+        cnx = connect_db()
+        cursor = cnx.cursor()
+        cursor.execute(' '.join(sql.split()), param)
+        res = cursor.fetchall()
+        close_db(cursor, cnx)
+        if not res:
+            return redirect('/admin')
+        return render_template('admin_user.html',
+            row=res[0],
+            user_name=session['user_name'])
+
+    def post(self):
+        user_id = request.args.get('user_id')
+        sql = '''
+            update user
+            set
+                MingCheng=%(name)s,ZhangHao=%(account)s,MiMa=%(pwd)s
+            where
+                id=%(user_id)s
+        '''
+        param = {
+            'name': request.form['name'],
+            'account': request.form['account'],
+            'pwd': request.form['password'],
+            'user_id': user_id
+        }
+        cnx = connect_db()
+        cursor = cnx.cursor()
+        cursor.execute(' '.join(sql.split()), param)
+        cnx.commit()
+        close_db(cursor, cnx)
+        return redirect('/admin/user?user_id=%s' % (user_id))
--- app.py
+++ app.py
@@ -17,11 +17,13 @@ app.host = '0.0.0.0'
 app.debug = True
 app.secret_key = 'Ovaphlow'
 app.config['UPLOAD_FOLDER'] = globalvars.G_UPLOAD_PATH
+
 app.add_url_rule('/',
     view_func=index.Index.as_view('index'))
 app.add_url_rule('/test', view_func=test.Test.as_view('test'))
 app.add_url_rule('/login', view_func=index.Login.as_view('login'))
 app.add_url_rule('/logout', view_func=index.Logout.as_view('logout'))
+
 app.add_url_rule('/chaxun', view_func=chaxun.ChaXun.as_view('chaxun'))
 app.add_url_rule('/chakan/<rec_id>', view_func=dangan.ChaKan.as_view('chakan'))
 app.add_url_rule('/daoru', view_func=shangchuan.DaoRu.as_view('daoru'))
@@ -53,7 +55,12 @@ app.add_url_rule('/dl_zip/<archieve_id>',
     view_func=dangan.DownloadZip.as_view('dl_zip'))
 app.add_url_rule('/tongji_month',
     view_func=chaxun.TongjiMonth.as_view('tongji_month'))
+
 app.add_url_rule('/admin', view_func=admin.Home.as_view('admin'))
+app.add_url_rule('/admin/user_list',
+    view_func=admin.UserList.as_view('admin user list'))
+app.add_url_rule('/admin/user', view_func=admin.User.as_view('admin user'))
+
 
 if __name__ == '__main__':
     app.run()
